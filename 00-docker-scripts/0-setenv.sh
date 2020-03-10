de6b705bb58b1c009b457b05afe11f16a587a51b
isuciu <isuciu0@uoc.edu>
2020-03-10 17:08:17 +0100

    removing folders that were moved to /old

--- 00-docker-scripts/0-setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=../../ado-tmp-data
-mkdir -p ${DATA_DIR}/mosquitto/data ${DATA_DIR}/mosquitto/log ${DATA_DIR}/influxdb ${DATA_DIR}/grafana
-
--- 00-docker-scripts/1-grant-permission.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DATA_DIR=../../ado-tmp-data
-chown -R 1883:1883 ${DATA_DIR}/mosquitto
-chown -R 472:472 ${DATA_DIR}/grafana
